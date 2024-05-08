aaa36d34739e52a5f3531338751fe2e90d6ba8c4
Xue Ming Chen <xueming.chen@tu-dortmund.de>
2024-05-08 20:57:56 +0200

    updated player script with Jumping

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -11,9 +11,17 @@ public class PlayerController : MonoBehaviour
 {
     // movementspeed from the character 
     [SerializeField]private float speed; 
-    private Rigidbody2D rigidbody2D; 
 
+    // rigidbody 2d to move the Player
+    private new Rigidbody2D rigidbody2D; 
+
+    // boolean for the look direction 
+    private bool facingRight = false;
+
+    // boolean to check the Player are under a ground or not  
     bool grounded; 
+
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -21,8 +29,33 @@ public class PlayerController : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        rigidbody2D.velocity = new Vector2(Input.GetAxis("Horizontal")*speed,rigidbody2D.velocity.y);
+    void FixedUpdate()
+    {   //grounded = Physics2D.OverlapCircle(groundCheck.position);  
+        float moveInput = Input.GetAxis("Horizontal");
+        rigidbody2D.velocity = new Vector2(moveInput*speed,rigidbody2D.velocity.y);
+        if(Input.GetKey(KeyCode.Space)){
+            if(grounded){
+            rigidbody2D.velocity = Vector2.up*speed;
+            grounded = false; 
+            }
+        }
+        if(facingRight==false && moveInput>0){
+            Flip();
+        }
+        else if (facingRight==true && moveInput<0){
+             Flip();
+        }
+    }
+
+    void Flip(){
+        facingRight = !facingRight;
+        Vector3 Scaler = transform.localScale;
+        Scaler*= -1; 
+        transform.localScale = Scaler;
+    }
+
+    void OnCollisionEnter2D(Collision2D other){
+        Debug.Log(other.gameObject.name);
+        grounded=true; 
     }
 }
