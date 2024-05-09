7771037db6d3f87fd879f368036ef588ee5465ee
Xue Ming Chen <xueming.chen@tu-dortmund.de>
2024-05-09 22:30:43 +0200

    Updated Jump method

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,9 +1,8 @@
 using System;
 using System.Collections;
-using System.Collections.Generic;
-using System.Data.SqlTypes;
-using Unity.VisualScripting;
+using Unity.VisualScripting.ReorderableList.Element_Adder_Menu;
 using UnityEngine;
+using UnityEngine.Experimental.GlobalIllumination;
 
 /*
 The PlayerController is to control the Actual player with WASD controlling  
@@ -27,7 +26,7 @@ public class PlayerController : MonoBehaviour
 
     //Dash implemented variables 
     private bool canDash = true ;
-    private bool isDashing;
+    private bool isDashing ;
 
     [SerializeField] private float dashingTime = 0.2f;
 
@@ -35,32 +34,67 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private float  dashsingPower=24f; 
 
     [SerializeField] private TrailRenderer tr; 
+
+    //gravtiy 
+
+    [Header("Gravity")]
+    public float multGravity = 9.81f;
+
+    public  float maxFallSpeed = 12; 
+
+      //variables all for wall Jump 
+    [Header("Ground Check")]
+    public Transform groundCheckPos; 
+
+    public Vector2 groundCheckSize = new Vector2(1.7f, 0.24f);
+    public LayerMask groundCheckLayer;
+
+    //variables all for wall Jump 
+    [Header("Wall Check")]
+    public Transform wallCheckPos; 
+
+    public Vector2 wallCheckSize = new Vector2(0.49f, 0.03f);
+    public LayerMask WallCheckLayer;
+
+    [Header("Wall Movement")]
+    public float WallSlideSpeed = 2;
+    bool isWallSliding; 
     
+    bool isWallJumping;
+    public float WallJumpDirection; 
+    float WallJumpTime;
+    float WallJumpTimer; 
+    public Vector2 WallJumpPower = new Vector2(5f,10f); 
+
+    public float wallJumpDuration = 2f;
+
     // Start is called before the first frame update
     void Start()
     {
         rigidbody2D = GetComponent<Rigidbody2D>();
+        tr = GetComponent<TrailRenderer>();
     }
-
-    // Update is called once per frame
-    void FixedUpdate()
-    {   //grounded = Physics2D.OverlapCircle(groundCheck.position);  
+    void Update(){
+        Jump();
         float moveInput = Input.GetAxis("Horizontal");
-        rigidbody2D.velocity = new Vector2(moveInput*speed,rigidbody2D.velocity.y);
-        if(Input.GetKey(KeyCode.E) && canDash){
-            StartCoroutine(Dash()); 
-        }
-        if(Input.GetKey(KeyCode.Space)){
-            if(grounded){
-            rigidbody2D.velocity = Vector2.up*JumpForce;
-            grounded = false; 
-            }
-        }
         if(facingRight==false && moveInput<0){
             Flip();
         }
         else if (facingRight==true && moveInput>0){
              Flip();
+        }
+        
+    }
+    // Update is called once per frame
+    void FixedUpdate()
+    {   //grounded = Physics2D.OverlapCircle(groundCheck.position); 
+            processWallSliding();
+            ProcessWallJumping(); 
+            float moveInput = Input.GetAxis("Horizontal");
+            rigidbody2D.velocity = new Vector2(moveInput*speed,rigidbody2D.velocity.y);
+
+        if(Input.GetKey(KeyCode.E) && canDash){
+            StartCoroutine(Dash()); 
         }
     }
 
@@ -70,12 +104,19 @@ public class PlayerController : MonoBehaviour
         Scaler.x*= -1; 
         transform.localScale = Scaler;
     }
-
-    void OnCollisionEnter2D(Collision2D other){
-        Debug.Log(other.gameObject.name);
-        grounded=true; 
+    
+    void Jump(){
+        if(Input.GetKey(KeyCode.Space)){
+            Debug.Log("Jumped");
+            if(grounded){
+            rigidbody2D.velocity =  new Vector2(rigidbody2D.velocity.x,JumpForce);
+            Debug.Log("Jump Velocity: " + rigidbody2D.velocity);
+            grounded = false; 
+            }
+        }
+        grounded = Physics2D.OverlapBox(groundCheckPos.position,groundCheckSize,0,groundCheckLayer);
+        Debug.Log("grounded : "+grounded);
     }
-
     private IEnumerator Dash(){
         canDash = false; 
         isDashing = true;
@@ -86,8 +127,80 @@ public class PlayerController : MonoBehaviour
         yield return new WaitForSeconds(dashingTime);
         tr.emitting = false;
         rigidbody2D.gravityScale = originalGravity;
+        isDashing = false; 
         yield return new WaitForSeconds(dashingCooldown);
         canDash= true;
-        isDashing= false;
     }
+
+    void processGravity(){
+        if (rigidbody2D.velocity.y > 0){
+            float baseGravity= rigidbody2D.gravityScale;
+            rigidbody2D.gravityScale = baseGravity*multGravity; 
+            rigidbody2D.velocity = new Vector2(rigidbody2D.velocity.x,Mathf.Max(rigidbody2D.velocity.y,-maxFallSpeed)); 
+        }
+    }
+
+    private void processWallSliding(){
+        if (grounded==false && WallCheck()&&Input.GetAxis("Horizontal")!=0){
+            isWallSliding = true;
+            rigidbody2D.velocity = new Vector2(rigidbody2D.velocity.x,Mathf.Max(rigidbody2D.velocity.y,-WallSlideSpeed));
+            Debug.Log("Wall sliding velocity: " + rigidbody2D.velocity.ToString());
+        }
+        else{
+            isWallSliding = false;
+        }
+        Debug.Log("isWallSliding: " + isWallSliding); 
+    }
+
+    //Gizmo color by selecting the separating the boxColliders 
+    void OnDrawGizmoSelected(){
+        Gizmos.color = Color.yellow; 
+        Gizmos.DrawWireCube(wallCheckPos.position, wallCheckSize);
+        }
+    
+    private bool WallCheck() {
+        return Physics2D.OverlapBox(wallCheckPos.position,wallCheckSize,0,WallCheckLayer); 
+    }
+
+    private void WallJump( ){
+        if(isWallSliding){
+            isWallJumping = false; 
+            WallJumpDirection = -transform.localScale.x;
+            WallJumpTimer = WallJumpTime;
+        }
+        else{
+            WallJumpTimer -=Time.deltaTime; 
+        }
+    }
+
+    private void ProcessWallJumping(){
+        if(isWallSliding){
+            isWallJumping = false; 
+            WallJumpDirection = -transform.localScale.x;
+            WallJumpTimer = WallJumpTime;
+            CancelInvoke(nameof(CancelWallJump));
+        }
+        else{
+            WallJumpTimer -= Time.deltaTime; 
+
+        }
+
+        if(WallJumpTimer >0f && Input.GetKeyDown(KeyCode.Space)){
+                isWallJumping = true; 
+                rigidbody2D.velocity = new Vector2(WallJumpDirection*WallJumpPower.x,WallJumpPower.y); 
+                WallJumpTimer = 0f; 
+        }
+
+        /*if(transform.localScale.x != WallJumpDirection){
+            facingRight = !facingRight;
+            Vector3 localScale = transform.localScale;
+            localScale.x *=-1f;
+            transform.localScale = localScale;
+        }*/
+        Invoke(nameof(CancelWallJump),wallJumpDuration);
+    }
+    private void CancelWallJump(){
+        isWallJumping = false;
+    }
+    
 }
