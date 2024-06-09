eec056f51cf39cda1aeb95a3dc91aec8f913a69f
Moncif <Moncif_11@hotmail.de>
2024-06-09 20:07:40 +0200

    add "leave"- Button and IP- Input field

--- Assets/Scripts/NetworkManagerUI.cs
+++ Assets/Scripts/NetworkManagerUI.cs
@@ -1,7 +1,9 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using Unity.Netcode;
+using Unity.Netcode.Transports.UTP;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -9,16 +11,35 @@ public class NetworkManagerUI : MonoBehaviour {
     [SerializeField] private Button serverButton;
     [SerializeField] private Button hostButton;
     [SerializeField] private Button clientButton;
+    [SerializeField] private Button leaveButton;
+    [SerializeField] private TMP_InputField IPAdressInput;
+    [SerializeField] private NetworkManager networkManager;
+    private String ipAdress = "";
 
     private void Awake() {
         serverButton.onClick.AddListener(() => {
             NetworkManager.Singleton.StartServer();
         });
         hostButton.onClick.AddListener(() => {
+            ipAdress = IPAdressInput.text;
+            if (ipAdress == "") {
+                ipAdress = "127.0.0.1";
+            }
+            Debug.Log(ipAdress);
+            networkManager.GetComponent<UnityTransport>().ConnectionData.Address=ipAdress;
             NetworkManager.Singleton.StartHost();
         });
         clientButton.onClick.AddListener(() => {
+            ipAdress = IPAdressInput.text;
+            if (ipAdress == "") {
+                ipAdress = "127.0.0.1";
+            }
+            Debug.Log(ipAdress);
+            networkManager.GetComponent<UnityTransport>().ConnectionData.Address=ipAdress;
             NetworkManager.Singleton.StartClient();
+        });
+        leaveButton.onClick.AddListener(() => {
+            NetworkManager.Singleton.Shutdown();
         });
     }
 }
--- Assets/Scripts/capePlayerController.cs
+++ Assets/Scripts/capePlayerController.cs
@@ -82,10 +82,9 @@ public class CapePlayerController : NetworkBehaviour
 
     void FixedUpdate()
     {
-        /*if (!IsOwner) {
+        if (!IsOwner && SceneManager.GetActiveScene().name == "Lobby") {
             return;
         }
-        */
         //moveInput = joystick.Horizontal;
         
             if (Input.GetAxisRaw("Horizontal") == 0 && Input.GetKey(KeyCode.A))
@@ -199,10 +198,10 @@ public class CapePlayerController : NetworkBehaviour
 
     void Update()
     {
-        /*if (!IsOwner) {
+        if (!IsOwner && SceneManager.GetActiveScene().name == "Lobby") {
             return;
         }
-        */
+        
         lastOnGroundTime += Time.deltaTime;
         jump = false;
         for (int i = 0; i < Input.touchCount; i++)
@@ -284,13 +283,11 @@ public class CapePlayerController : NetworkBehaviour
             {
                 transform.eulerAngles = new Vector3(0, 0, 0);
                 direction = "right";
-            //    GetComponent<NetworkTransform>().Teleport(transform.localPosition, transform.localRotation, transform.localScale);
             }
             else if (moveInput < 0 && direction != "left")
             {
                 transform.eulerAngles = new Vector3(0, 180, 0);
                 direction = "left";
-               // GetComponent<NetworkTransform>().Teleport(transform.localPosition, transform.localRotation, transform.localScale);
             }   
         }
 
